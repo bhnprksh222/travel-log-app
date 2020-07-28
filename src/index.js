5215bd5f20cf7041ac1a8b2086cc9d369991feae
Bhanu Prakash <bhnprksh222@gmail.com>
2020-07-29 02:07:01 +0530

    linekd db

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
 
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-  origin: 'http//localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 app.get('/', (req, res) => {
