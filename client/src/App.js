17d975fb527e1b1d35949e2b15e128b500855ced
Bhanu Prakash <bhnprksh222@gmail.com>
2020-07-29 17:46:35 +0530

    Added marker 📍 and some functionalities✨

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import  listLogEntries  from './API';
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState([]);
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -24,12 +25,21 @@ const App = () => {
 
   }, []);
 
+  const showAddMarkerPopup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    })
+  };
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle='mapbox://styles/bhnprksh222/ckd77zqx6012m1jrosf6e06ks'
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPopup}
     >
       {
         logEntries.map(entry => (
@@ -41,22 +51,28 @@ const App = () => {
             >
               <div
                 onClick={() => setShowPopup({
-                  ...showPopup,
+                  // ...showPopup,
                   [entry._id]: true,
                 })}
                 style={{
                   cursor: 'pointer'
                 }}
               >
-                  <img 
-                    className="marker" 
-                    style={{
-                      height: `${6 * viewport.zoom}`,
-                      width: `${6 * viewport.zoom}`
-                    }}
-                    src="https://i.imgur.com/y0G5YTX.png" 
-                    alt="marker" 
-                  />
+                <svg
+                  className="marker yellow"
+                  style={{
+                    height: `${6 * viewport.zoom}px`,
+                    width: `${6 * viewport.zoom}px`,
+                  }}
+                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                  <g>
+                    <g>
+                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                    </g>
+                  </g>
+                </svg>
               </div>
             </Marker>
             {
@@ -66,20 +82,59 @@ const App = () => {
                   longitude={entry.longitude}
                   closeButton={true}
                   closeOnClick={false}
-                  onClose={() => setShowPopup({
-                    ...showPopup,
-                    [entry._id]: false,
-                  })}
+                  dynamicPosition={true}
+                  onClose={() => setShowPopup({})}
                   anchor="top" >
                   <div className="popup">
                     <h3>{entry.title}</h3>
                     <p>{entry.comments}</p>
+                    <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
                   </div> 
                 </Popup>
               ) : null
             }
           </>
         ))
+      }
+      {
+        addEntryLocation ? (
+          <>
+            <Marker 
+                latitude={addEntryLocation.latitude} 
+                longitude={addEntryLocation.longitude} 
+            >
+              <div>
+                <svg
+                  className="marker red"
+                  style={{
+                    height: `${6 * viewport.zoom}px`,
+                    width: `${6 * viewport.zoom}px`,
+                  }}
+                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                  <g>
+                    <g>
+                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </Marker>
+            <Popup
+              latitude={addEntryLocation.latitude} 
+              longitude={addEntryLocation.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setAddEntryLocation(null)}
+              anchor="top" >
+              <div className="popup">
+                <h3>Add your new log entry!</h3>
+              </div> 
+            </Popup>
+          </>
+        ) : null
       }
     </ReactMapGL>
   );
